ea1534b7c855883b8b32309068530043be8f3669
Marco Kuidja <kuidjamarco@gmail.com>
2024-06-18 00:25:04 +0100

    feat: #5 adjusted exceptions filter

--- src/all-exceptions.filter.ts
+++ src/all-exceptions.filter.ts
@@ -4,6 +4,7 @@ import {
   ExceptionFilter,
   HttpException,
   HttpStatus,
+  Logger,
 } from "@nestjs/common";
 import { HttpAdapterHost } from "@nestjs/core";
 
@@ -12,24 +13,21 @@ export class AllExceptionsFilter implements ExceptionFilter {
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
   catch(exception: unknown, host: ArgumentsHost): void {
+    Logger.error(exception, AllExceptionsFilter.name);
     // In certain situations `httpAdapter` might not be available in the
     // constructor method, thus we should resolve it here.
     const { httpAdapter } = this.httpAdapterHost;
 
     const ctx = host.switchToHttp();
-
     const httpStatus =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    const httpError =
-      exception instanceof HttpException
-        ? exception.message
-        : "Internal server error";
-
     const responseBody = {
-      message: httpError,
+      ...(exception instanceof HttpException
+        ? (exception.getResponse() as object)
+        : { message: "Internal server error" }),
       statusCode: httpStatus,
       timestamp: new Date().toISOString(),
       path: httpAdapter.getRequestUrl(ctx.getRequest()),
